e31d877542da2024f3de4ea4a48e94aa7e1c5cae
Senthilkumar <sskumar.muthupet@gmail.com>
2022-10-27 18:05:42 +0530

    Updated reset password style

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -15,11 +15,11 @@ function ResetPassword() {
 
         <section className="bg-gradient-to-b from-gray-100 to-white">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
-            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+            <div className="pt-32 pb-6 md:pt-40 md:pb-20">
 
               {/* Page header */}
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h1 className="h1 mb-4">Let’s get you back up on your feet</h1>
+                <h1 className="h1 mb-2">Let’s get you back up on your feet</h1>
                 <p className="text-xl text-gray-600">Enter the email address you used when you signed up for your account, and we’ll email you a link to reset your password.</p>
               </div>
 
@@ -28,7 +28,7 @@ function ResetPassword() {
                 <form>
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
+                      <label className="inline-block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
                       <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
                     </div>
                   </div>
